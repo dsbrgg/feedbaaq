66cccc8918e0af659de43aecfdfc48d46c1f9b2e
DB <dsbrgg@gmail.com>
2017-09-03 15:56:32 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+// hbs.registerPartials(__dirname + '/views/partials');
+// app.set('view engine', 'hbs');
+// app.use(express.static(__dirname + '/public'));
+
+// app.use((req, res, next) => {
+// 	var now = new Date().toString();
+// 	var log = `${now} : ${req.method}  ${req.url}`;
+//
+// 	fs.appendFile('server.log', log + '\n', (err) => {
+// 		if(err) console.log('Unable to append server.log');
+// 	});
+// 	console.log(log);
+// 	next();
+// });
+
+// hbs.registerHelper('getCurrentYear', () => {
+// 	return new Date().getFullYear();
+// });
+
+app.get('/', (req, res) => {
+	res.send('<h1>OXENTE</h1>');
+	// res.render('home.hbs', {
+	// 	title : 'Home Page',
+	// 	welcomeMessage : 'Whats up'
+	// });
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		error : 'Bad request'
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
